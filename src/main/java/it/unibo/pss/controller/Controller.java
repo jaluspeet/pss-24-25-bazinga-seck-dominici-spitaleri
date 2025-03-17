8293c943eea3918e5abcb58498943784cd59d842
AleBillo <alessandro.dominici2@studio.unibo.it>
2025-03-17 16:05:39 +0100

    forgot controller comments

--- src/main/java/it/unibo/pss/controller/Controller.java
+++ src/main/java/it/unibo/pss/controller/Controller.java
@@ -10,34 +10,48 @@ import it.unibo.pss.view.View;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * Controller class that manages the communication between the Model and the View
+ * in order to keep strict MVC separation.
+ *
+ * The Controller class is responsible for:
+ * - creating the Model and the View
+ * - updating the View when the Model changes
+ * - updating the Model when the View sends commands
+ * - starting the application
+ */
 public class Controller extends Application implements ModelObserver, ViewObserver {
 	private Model model;
 	private View view;
 
+	/**
+	 * Start the application by creating the Model and the View.
+	 *
+	 * @param stage the primary stage for the application
+	 */
 	@Override
 	public void start(Stage stage) {
 		this.model = new Model(SharedConstants.WORLD_WIDTH, SharedConstants.WORLD_HEIGHT);
 		this.model.addObserver(this);
-
-		this.view = new View(
-				stage,
-				SharedConstants.WINDOW_TITLE,
-				SharedConstants.WINDOW_WIDTH,
-				SharedConstants.WINDOW_HEIGHT,
-				this
-				);
+		this.view = new View(stage, SharedConstants.WINDOW_TITLE, SharedConstants.WINDOW_WIDTH, SharedConstants.WINDOW_HEIGHT, this );
 	}
 
+	/**
+	 * Update the View when the Model changes.
+	 */
 	@Override
 	public void onModelUpdated() {
 		ModelDTO latestDTO = model.getLatestModelDTO();
 		view.updateModel(latestDTO);
 	}
 
-	public ModelDTO getLatestModelDTO() {
-		return model.getLatestModelDTO();
-	}
+	public ModelDTO getLatestModelDTO() { return model.getLatestModelDTO(); }
 
+	/**
+	 * Update the Model when the View sends commands.
+	 *
+	 * @param viewDTO the command sent by the View
+	 */
 	@Override
 	public void onViewAction(ViewDTO viewDTO) {
 		switch (viewDTO.getCommand().getType()) {
@@ -59,23 +73,16 @@ public class Controller extends Application implements ModelObserver, ViewObserv
 		}
 	}
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+	public Model getModel() { return model; }
+	public void setModel(Model model) { this.model = model; }
+	public View getView() { return view; }
+	public void setView(View view) { this.view = view; }
 
-	public Model getModel() {
-		return model;
-	}
+	/**
+	 * Start the application.
+	 *
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) { launch(args); }
 
-	public void setModel(Model model) {
-		this.model = model;
-	}
-
-	public View getView() {
-		return view;
-	}
-
-	public void setView(View view) {
-		this.view = view;
-	}
 }
